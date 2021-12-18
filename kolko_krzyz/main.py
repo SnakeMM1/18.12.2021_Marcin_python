330986de429d6e2005476360eb26ea6c1c6795a4
SnakeMM1 <marcin.krasuski@hotmail.com>
2021-12-18 10:03:24 +0100

    Plansz plus klawisze

--- kolko_krzyz/main.py
+++ kolko_krzyz/main.py
@@ -1,4 +1,16 @@
 #%%
 # Tutaj będzie główny kod gry.
 
-PlanszaDoGry = {}
+PlanszaDoGry = {'7':' ','8':' ','9':' ',
+                '4':' ','5':' ','6':' ',
+                '1':' ','2':' ','3':' ',
+                }
+
+KlawiszeGry = []
+
+for i in PlanszaDoGry:
+    KlawiszeGry.append(i)
+print(KlawiszeGry)
+
+
+# %%
