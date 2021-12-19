aa78ca6c557b2b8ea9154701ed7ea4e9e915001d
SnakeMM1 <marcin.krasuski@hotmail.com>
2021-12-19 08:33:46 +0100

    1

---
+++ Module/analiza.py
@@ -0,0 +1,4 @@
+from moduls import calculate_max
+
+calculate_max([5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+
---
+++ Module/moduls.py
@@ -0,0 +1,11 @@
+#%%
+
+
+def calculate_average(x):
+    return sum(x) / len(x)
+
+def calculate_min (x):
+    return min(x)
+
+def calculate_max (x):
+    return max(x)
