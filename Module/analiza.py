f3f0eb5de769dbb9622a742e8b91c434b38bd7a0
SnakeMM1 <marcin.krasuski@hotmail.com>
2021-12-19 09:10:26 +0100

    2

--- Module/analiza.py
+++ Module/analiza.py
@@ -1,4 +1,9 @@
-from moduls import calculate_max
+import moduls
+#%%
+print(moduls.calculate_max([5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]))
 
-calculate_max([5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+print(moduls.calculate_min([5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]))
 
+print(moduls.calculate_average([5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]))
+
+# %%
---
+++ Module/analiza_pakiet.py
@@ -0,0 +1,8 @@
+import pakiet.modul_w_pakiecie
+
+print(pakiet.modul_w_pakiecie.funkcja(5))
+
+
+from pakiet.modul_w_pakiecie import funkcja
+
+print(funkcja(10))
